44f091bd7ca0807f31c02bcb57a54273038b3c24
AtMyCode <zhanglijian2013@163.com>
2014-10-18 11:09:00 +0800

    Initial Commit

---
+++ DayDayWeibo/KZJAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  KZJAppDelegate.h
+//  DayDayWeibo
+//
+//  Created by Ibokan on 14-10-18.
+//  Copyright (c) 2014年 KZJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KZJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ DayDayWeibo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DayDayWeibo
+//
+//  Created by Ibokan on 14-10-18.
+//  Copyright (c) 2014年 KZJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KZJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KZJAppDelegate class]));
+    }
+}
